436f6ea4dba7d47dcad659195a63c9fddba4fe10
AzuosOvatsug <gustavo.ifto@gmail.com>
2019-04-18 10:53:38 -0300

    Inclusão do CRUD, Paginação e CORS

--- server.js
+++ server.js
@@ -1,28 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
+const cors = require('cors');
 
 // Iniciando App
 const app = express();
+app.use(express.json());
+app.use(cors());
 
 // Iniciando o DB
 mongoose.connect(
     'mongodb://localhost:27017/nodeapi',
-    { useNewUrlParser: true });
-requireDir("src/models/");
+    { useNewUrlParser: true }
+);
+requireDir("./src/models");
 
-// Recebendo model
-const Product = mongoose.model('Product');
+// Rotas
+app.use("/api", require("./src/routes"));
 
-// Primeira rota
-app.get('/', (req, res) => {
-    Product.create({
-        title: 'Gustavo',
-        description: 'Teste com node-api',
-        url:'https://github.com/AzuosOvatsug/node-api'
-    });
-
-
-    res.send("Ta funfando?");
-});
 app.listen(3001);
